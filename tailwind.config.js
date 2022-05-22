e6f3e83e19b5853ded03095a27af71c591845235
aditya-exe <47419610+aditya-exe@users.noreply.github.com>
2022-05-22 18:54:35 +0530

    Added custom styling

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        'title': ['Sansita', 'sans-serif'],
+        'body': ['Lato', 'sans-serif']
+      },
+      colors:{
+        'pri': '#081F4D',
+        'sec': '#0083FF',
+        'bg': '#D9E6FF',
+        'gen': '#3B4D71'
+      }
+    },
+  },
+  plugins: [],
+}
